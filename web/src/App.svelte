2a17d8b15ca9ba18a67c65f8ae56e36a878eb975
braydend <braydendiluca@gmail.com>
2023-08-25 12:30:39 +1000

    use secure websocket protocol

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -11,7 +11,7 @@
   $: score = 0;
 
   onMount(() => {
-    websocket = new WebSocket(`ws://${location.host}/ws/join`);
+    websocket = new WebSocket(`wss://${location.host}/ws/join`);
 
     websocket.onmessage = (e) => {
       console.log(e.data);
